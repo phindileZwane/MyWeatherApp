7955347fce9a14d9e9ef9467d0c1fa98807e6321
Phindile <pzwane124@gmail.com>
2023-12-22 09:19:57 +0200

    finished with UI

---
+++ index.js
@@ -0,0 +1,19 @@
+let input = document.getElementById('search');
+function myFunction(){
+    window.location = "popUp.html"
+
+   getCoordinates();
+   
+}
+
+
+function getCoordinates(){
+    var btn = document.getElementById('button')
+    var address = document.getElementById('address').value;
+    var theUrl = "https://geocoder.1s.hereapi.com/6.2/geocode.json/searchtext=" + address +"&gen=9&apiKey={key goes here}";
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    var json = JSON.parse(xmlHttp.responseText);
+    console.log(JSON);
+}
