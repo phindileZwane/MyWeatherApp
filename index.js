5ce8823f3db2098fc8276fd9a72329c439e32fa6
Phindile <pzwane124@gmail.com>
2023-12-27 21:57:25 +0200

    starting with java

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-//let input = document.getElementById('search');
-//function myFunction(){
-   // window.location = "popUp.html"
+const searchInput = document.getElementById("search")
+const btn = document.getElementById("button")
+const locations = document.getElementById("place")
+const icon = document.getElementById("icon")
+const description = document.getElementById("description")
+const temp = document.getElementById("temperature")
 
- //  getCoordinates();
-   
-//}
+const apiKey = "3e40b7da38ff873d21a3423887f04809"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather"
 
+btn.addEventListener("click", () =>{
+  const place = searchInput.value;
+  if (place){
+    getWeather(place)
+  }
+})
+function getWeather(place){
 
-//function getCoordinates(){
-   // var btn = document.getElementById('button')
-   // var address = document.getElementById('address').value;
-    //var theUrl = "https://geocoder.1s.hereapi.com/6.2/geocode.json/searchtext=" + address +"&gen=9&apiKey={key goes here}";
-  //  var xmlHttp = new XMLHttpRequest();
-   // xmlHttp.open("GET", theUrl, false);
-   // xmlHttp.send(null);
-   // var json = JSON.parse(xmlHttp.responseText);
-   // console.log(JSON);
-//}
+  const url = `${apiUrl}?q=${place}&appid=${apiKey}&units=metric`
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    locations.textContent = data.name;
+    icon.textContent = data.weather[0].icon;
+    description.textContent = data.weather[0].description
+    temp.textContent = `${Math.round(data.main.temp) + "C"}`
+
+  })
+  .catch(error => {
+    console.error("Error while fectchong data:", error);
+  })
+}
 
