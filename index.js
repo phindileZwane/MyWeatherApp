830138dde4862999aa1f86dba7bb9a683bcaa483
Phindile <pzwane124@gmail.com>
2023-12-30 17:21:29 +0200

    tryin to fix the hourl but not working

--- index.js
+++ index.js
@@ -1,34 +1,66 @@
-const searchInput = document.getElementById("search")
-const btn = document.getElementById("button")
-const locations = document.getElementById("place")
-const icon = document.getElementById("icon")
-const description = document.getElementById("description")
-const temp = document.getElementById("temperature")
+const apiKey = '3e40b7da38ff873d21a3423887f04809';
 
-const apiKey = "3e40b7da38ff873d21a3423887f04809"
-const apiUrl = "https://api.openweathermap.org/data/2.5/forecast"
 
-btn.addEventListener("click", () =>{
-  const place = searchInput.value;
-  if (place){
-    getWeather(place)
-  }
-})
+
+//main weather
+const townInput = document.getElementById('search');
+const button = document.getElementById('button');
+const town = document.getElementById('place');
+const mainIcon = document.getElementById('mainIcon');
+const description = document.getElementById('description');
+const temperature = document.getElementById('temperature');
+
+//forecast weather
+const smallIcons = document.getElementById('small-icons')
+const smallImg = document.getElementById('small-img');
+const time = document.getElementById('timezone');
+const smallTemp = document.getElementById('small-temp');
+
+//creating an alart if input is empty
+button.addEventListener('click', () => {
+  const place = search.value;
+if (place){
+  getWeather(place);
+}
+});
+//fetching data from open weather map
 function getWeather(place){
+  const currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${place}&appid=${apiKey}`;
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${apiKey}`;
 
-  const url = `${apiUrl}?q=${place}&appid=${apiKey}&units=metric`
+  fetch(currentUrl)
+    .then(response => response.json())
+    .then(data => {
+      town.textContent = data.name;
+      mainIcon.textContent = data.weather[0].icon;
+      const iconUrl = `https://openweathermap.org/img/wn/${mainIcon}.png`;
+      temperature.textContent = `${Math.round(data.main.temp)}°`
+      description.textContent = data.weather[0].description;
+    })
+    .catch(error => {
+      console.error('Error while Fetching current weather:', error)
+    });
 
-  fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    locations.textContent = list.name;
-    icon.textContent = data.weather[0].icon;
-    description.textContent = data.weather[0].description
-    temp.textContent = `${Math.round(data.main.temp) + "°" + "C"}`
+  fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => {
+     displayHourly(data.list)
+   })
+    .catch(error =>{
+      console.error('Error while fecthing forecast weather:', error)
+      alert('Error while getting hourly data')
+    });
+    function displayHourly(hourlyData){
+      const upComing24Hours = hourlyData.slice(0, 8);
 
-  })
-  .catch(error => {
-    console.error("Error while fectchong data:", error);
-  })
+      upComing24Hours.forEach(item => {
+        time = new Date(item.dt * 1000);
+        smallTemp.textContent = Math.round(item.main.temp)
+        smallImg = item.weather[0].icon;
+        const smallImgUrl = `https://openweathermap.org/img/wn/${smallImg}.png`;
+
+
+      })
+    }
 }
 
